8d7f46348622444454935531967fbd4ba0da394b
Yannik Methot <yannik.methot@nurun.com>
2014-10-07 22:37:37 -0400

    add source files

---
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/AndroidManifest.xml \
---
+++ platforms/android/ant-build/classes.dex.d
@@ -0,0 +1,9 @@
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/classes.dex : \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/classes/com/danzo/gym/BuildConfig.class \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/classes/com/danzo/gym/DanzoGym.class \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/classes/com/danzo/gym/R$attr.class \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/classes/com/danzo/gym/R$drawable.class \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/classes/com/danzo/gym/R$string.class \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/classes/com/danzo/gym/R$xml.class \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/classes/com/danzo/gym/R.class \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/dexedLibs/classes-4f192646532d13959d88137440cef871.jar \
