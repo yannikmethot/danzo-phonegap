d31d44d45d310357beaf09baf5c06275255b504f
Yannik Methot <yannik.methot@nurun.com>
2014-10-14 23:46:23 -0400

    sign android app

---
+++ platforms/android/ant-build/DanzoGym-release-unsigned.apk.d
@@ -0,0 +1,4 @@
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/DanzoGym-release-unsigned.apk : \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/DanzoGym.ap_ \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/ant-build/classes.dex \
+/Users/yannik.methot/Desktop/DanzoGym/native-app/platforms/android/CordovaLib/ant-build/classes.jar \
